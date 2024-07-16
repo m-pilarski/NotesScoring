38a97b763f4bccdea041ee956f086bfa7b9410f4
m-pilarski <pilarski@cryptolab.net>
2024-07-16 20:09:00 +0200

    update

---
+++ code/download_data.R
@@ -0,0 +1,67 @@
+.data_dir <- "~/Documents/Research/NoteScoring/data/"
+
+download_dataset <- function(.dataset, .date, .chunk, .data_dir){
+  
+  .dataset <<- .dataset
+  .date <<- .date
+  .chunk <<- .chunk
+  .data_dir <<- .data_dir
+  
+  stopifnot(
+    .dataset %in% c("notes", "ratings", "noteStatusHistory", "userEnrollment"),
+    lubridate::is.POSIXct(.date),
+    is.numeric(.chunk) & .chunk < 100000
+  )
+  
+  .date_path <- strftime(.date, "%Y/%m/%d")
+  
+  .data_path_rel <- 
+    .dataset |> 
+    dplyr::case_match(
+      "notes" ~ "notes/notes",
+      "ratings" ~ "noteRatings/ratings",
+      "noteStatusHistory" ~ "noteStatusHistory/noteStatusHistory",
+      "userEnrollment" ~ "userEnrollment/userEnrollment"
+    ) |> 
+    paste0("-", sprintf("%05d", .chunk), ".tsv")
+
+  .resp_full <- 
+    httr2::request("https://ton.twimg.com/birdwatch-public-data") |> 
+    httr2::req_url_path_append(.date_path, .data_path_rel) |>
+    httr2::req_error(is_error = \(...){FALSE}) |>
+    httr2::req_perform()
+  
+  .resp_status <- httr2::resp_status(.resp_full)
+  
+  if(.resp_status == 200){
+    
+    .data_path_local <- fs::path(.data_dir, .data_path_rel)
+    
+    fs::dir_create(fs::path_dir(.data_path_local))
+    
+    .resp_full |> 
+      httr2::resp_body_raw() |> 
+      readr::write_file(path=.data_path_local)
+    
+  }
+  
+  return(.resp_status)
+  
+}
+
+download_database <- function(.date=Sys.time(), .data_dir=.data_dir){
+  .dataset_vec <- c("notes", "ratings", "noteStatusHistory", "userEnrollment")
+  for(.dataset in .dataset_vec){
+    .chunk <- 0
+    repeat{
+      .download_status <- download_dataset(
+        .dataset=.dataset, .date=.date, .chunk=.chunk, .data_dir=.data_dir
+      )
+      if(.download_status != 200){break}
+      .chunk <- .chunk + 1
+    }
+  }
+  fs::dir_tree(.data_dir)
+}
+
+download_database(.date=Sys.time(), .data_dir=.data_dir)
---
+++ code/setup_project.R
@@ -0,0 +1,93 @@
+################################################################################
+## INSTALL SETUP DEPENDENCIES ##################################################
+################################################################################
+
+if(!"remotes" %in% installed.packages()){install.packages("remotes")}
+remotes::install_github("m-pilarski/helprrr")
+
+################################################################################
+################################################################################
+################################################################################
+
+path_proj <- here::here()
+
+stopifnot(fs::path_file(path_proj) == "NotesScoring", fs::is_dir(path_proj))
+
+################################################################################
+################################################################################
+################################################################################
+
+path_libs <- fs::path(path_proj, "libs")
+
+path_conda_lib_exe <- try(fs::path(reticulate::conda_binary()))
+if(is(path_conda_lib_exe, "try-error")){
+  path_conda_lib <- fs::path(path_libs, "proj_miniconda")
+  reticulate::install_miniconda(path_conda_lib)
+  path_conda_lib_exe <- fs::path(path_conda_lib, "bin/conda")
+}else if(fs::file_exists(path_conda_lib_exe)){
+  path_conda_lib <- fs::path(path_conda_lib_exe, "../..")
+}else{
+  stop()
+}
+
+path_renv <- fs::path(path_libs, "proj_renv")
+
+path_conda_env <- fs::path(path_libs, "proj_conda_env")
+path_conda_env_py <- fs::path(path_conda_env, "bin/python")
+
+path_renv_lock <- fs::path(path_libs, "proj_renv.lock")
+path_conda_env_yml <- fs::path(path_libs, "proj_environment.yml")
+
+################################################################################
+################################################################################
+################################################################################
+
+helprrr::setenv_persist(
+  # FOR {renv}
+  RENV_PATHS_RENV=path_renv,
+  RENV_PATHS_LOCKFILE=path_renv_lock,
+  RENV_PATHS_CONDA_EXPORT=path_conda_env_yml,
+  RENV_CONFIG_SANDBOX_ENABLED="FALSE",
+  # FOR {arrow}
+  LIBARROW_BINARY="TRUE",
+  LIBARROW_MINIMAL="FALSE",
+  ARROW_USE_PKG_CONFIG="FALSE",
+  # FOR {V8}
+  DOWNLOAD_STATIC_LIBV8="1",
+  # ...
+  .path_proj=path_proj
+)
+
+################################################################################
+################################################################################
+################################################################################
+
+writeLines(
+  text=fs::path_rel(path_libs, path_proj),
+  con=fs::path(".renvignore")
+)
+
+renv::init(project=path_proj, restart=FALSE, bare=TRUE)
+
+renv::hydrate(project=path_proj, prompt=FALSE)
+
+if(fs::dir_exists(path_conda_env)){fs::dir_delete(path_conda_env)}
+reticulate::conda_create(
+  envname=path_conda_env, conda=path_conda_lib_exe, python_version="3.9"
+)
+reticulate::conda_install(
+  envname=path_conda_env, 
+  conda=path_conda_lib_exe,
+  packages=readr::read_lines(
+    here::here("communitynotes/requirements.txt"), skip_empty_rows=TRUE
+  ),
+  pip=TRUE
+)
+
+renv::use_python(
+  python=path_conda_env_py, name=path_conda_env, project=path_proj, type="auto"
+)
+
+renv::snapshot(project=path_proj, prompt=FALSE)
+
+renv::activate(project=path_proj)
